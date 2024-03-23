09f9cd5c25284831c56af78f2c7425b7da6992d9
Priyanshu Mewada <oppriyanshu@gmail.com>
2024-03-23 18:31:41 +0530

    scroller and hover image effext ADDED

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let ok = false;
-console.log(Number(ok));
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
-arr =[1,2,3,4,5]
-arr.forEach(function(ele){
-    console.log(ele);
-    
+var container = document.querySelector("#elem-container");
+var fixed = document.querySelector("#fixed-image");
+
+container.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
 })
+container.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+
+elems.forEach(function(k){
+    k.addEventListener("mouseenter", function(){
+        let img = k.getAttribute("data-name");
+        fixed.style.backgroundImage = `url(${img})`;
+        
+    })
+});
+
